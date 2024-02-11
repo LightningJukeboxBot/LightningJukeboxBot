86b150073b0289c5e83514a787648a57394bbba1
Giacomo Gagliano <giacomo.gagliano@me.com>
2024-02-11 12:35:06 +0100

    imports links

--- web/index.js
+++ web/index.js
@@ -489,13 +489,10 @@ var createUI = (currentqueue, results) => {
   ] = createSearchResult(results);
   const [queue, votesCbs, setQueueBgColor, setVoteColor, setTracksInfoColor] = createQueue(currentqueue);
   const [footer, setFooterColor] = createFooter([
-    [makeIconPath("telegram-icon"), "urltelegram"],
-    [
-      makeIconPath("github-cion"),
-      "https://github.com/LightningJukeboxBot/LightningJukeboxBot"
-    ],
-    [makeIconPath("x-icon"), "urlx"],
-    [makeIconPath("nrfm-icon"), "urlnrfm"]
+    [makeIconPath("telegram-icon"), telegramLink],
+    [makeIconPath("github-cion"), githubLink],
+    [makeIconPath("x-icon"), xLink],
+    [makeIconPath("nrfm-icon"), nrfmLink]
   ]);
   const activeTab = "red-tab";
   setMargin0(body);
